226bdd8b2fc3fc63fa0187fd189dc4d9f3e08041
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 15:20:32 +0200

    Code 04

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ivesk du skaicius: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Suma: " + suma(a, b));
+        System.out.println("Skirtumas: " + skirtumas(a, b));
+    }
+
+        public static int suma(int a, int b) {
+        return a + b;
+    }
+
+        public static int skirtumas(int a, int b) {
+        return a - b;
     }
 }
